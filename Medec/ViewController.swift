49717f163eea71108a11b5132a2221c24ae811ff
Yeabsira Gashaw <yeab13me@gmail.com>
2019-10-29 23:45:35 +0300

    Initial Commit

---
+++ Medec/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Medec
+//
+//  Created by Yeabsira Gashaw on 10/29/19.
+//  Copyright © 2019 Yabbb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
